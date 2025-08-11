e22274e55f959e4e0e4ce506c838fac2a26346f5
Dazchen <guidazchen@gmail.com>
2025-08-11 13:43:08 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <><h1>Eu te amo Isadora! pra sempre sempre sempre...</h1><p1>me manda foto sua sorrindo?? hehe</p1></>
+  return <><h1>Eu te amo Isadora! pra sempre sempre sempre...❤️ </h1><p1>me manda foto sua sorrindo?? hehe</p1></>
 }
 
 export default Home;
